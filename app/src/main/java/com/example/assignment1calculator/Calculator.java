fde1cadba2fdd704b6c13808b3b8c3b176c0a7e0
lindamol <lindamoljosem@gmail.com>
2021-11-07 15:27:52 -0500

    Update Calc

--- app/src/main/java/com/example/assignment1calculator/Calculator.java
+++ app/src/main/java/com/example/assignment1calculator/Calculator.java
@@ -1,2 +1,81 @@
-package com.example.assignment1calculator;public class Calculator {
+package com.example.assignment1calculator;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class Calculator {
+    ArrayList<String> calArray = new ArrayList<>();
+    ArrayList<String> historyArray = new ArrayList<>();
+    double finalresult = 0;
+    double calresult;
+    int i = 0;
+    boolean error = false;
+
+    void pushtoArray(String value) {
+        calArray.add(value);
+    }
+     //History Array
+    void pushtoHistoryArray(String value) {
+        historyArray.add(value);
+    }
+
+    //iterate and calculate
+    double calculate() {
+        while (calArray.size() >= 3) { //Untill Array list <=3
+            //1+2+5+4
+           // if(calArray.get(1).charAt(0) == '+' || calArray.get(1).charAt(0) =='-' || calArray.get(1).charAt(0) == '*' || calArray.get(1).charAt(0) =='/')
+          //Array index for iteration is always 0,1,2, Because after each iteration first 3 array elements
+            // will be removed and //push the rest  to first indexes
+            double firstnum = (Double.parseDouble(calArray.get(0)));
+            double secondnum = (Double.parseDouble(calArray.get(2)));
+            String operator = calArray.get(1);
+            //pass the first 3 elements and get the Result
+            finalresult = getResult(firstnum, secondnum, operator);
+            //Remove First 3 elements
+            calArray.remove(0); // +2+5+4
+            calArray.remove(0);//2+5+4
+            calArray.remove(0);//+5+4
+            //Add the result to first index of the Arraylist,Replace the first index to result and do iteration
+            calArray.add(0, (Double.toString(finalresult))); // New arraylist : 3+5+4
+
+//        else{
+//            error = true; // if there is no operator set error as true;
+//            System.out.println("Format Error");
+//            System.exit(0);
+//        }
+        }
+        return finalresult;
+    }
+
+    // To perform each iteration in the arraylist
+    double getResult(double num1, double num2, String operator) {
+        switch (operator) {
+            case "+":
+                calresult = num1 + num2;
+                break;
+            case "-":
+                calresult = num1 - num2;
+                break;
+            case "/":
+                calresult = num1 / num2;
+                break;
+            case "*":
+                calresult = num1 * num2;
+                break;
+            default:
+                System.out.println("Invalid Entry, Please Check");
+                break;
+        }
+
+        return calresult;
+    }
+
+    void printarray() {
+        Log.d("my array is ", "Cal " + calArray.toString() + "");
+    }
+//    void printHistory(){
+//        Log.d("my History array is ","Cal "+historyArray.get(i)+"");
+//        i++;
+//    }
 }
--- app/src/main/java/com/example/assignment1calculator/MainActivity.java
+++ app/src/main/java/com/example/assignment1calculator/MainActivity.java
@@ -3,12 +3,110 @@ package com.example.assignment1calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
+    Calculator calobj = new Calculator();
+    TextView resultview;
+    Button buttonequal;
+    String text = "";
+    String histext = "";
+    double result = 0;
+    TextView historyText;
+    Button advancebutton;
+    Button buttonC;
+    int mode = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        historyText = findViewById(R.id.historyText);
+        resultview = findViewById(R.id.resultText);
+        resultview.setText("");
+        advancebutton = findViewById(R.id.buttonadvance);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button button0 = findViewById(R.id.button0);
+        buttonC = findViewById(R.id.buttonC);
+        buttonequal = findViewById(R.id.buttonequal);
+        Button buttonsub = findViewById(R.id.buttonminus);
+        Button buttonadd = findViewById(R.id.buttonplus);
+        Button buttonmultiply = findViewById(R.id.buttonmultiply);
+        Button buttondivide = findViewById(R.id.buttondivide);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        buttonC.setOnClickListener(this);
+        button0.setOnClickListener(this);
+        buttonequal.setOnClickListener(this);
+        buttonsub.setOnClickListener(this);
+        buttonadd.setOnClickListener(this);
+        buttonmultiply.setOnClickListener(this);
+        buttondivide.setOnClickListener(this);
+        advancebutton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button) v;
+        String buttontitle = button.getText().toString();
+        if(v == buttonC){
+            resultview.setText("");
+            text = "";
+            calobj.calArray.clear();
+            histext = "";
+        }
+        else if(v == advancebutton){
+            System.out.println("Mode value is"+mode+"");
+            if(mode ==0)//for Display History
+            {  mode=1;
+                advancebutton.setText("STANDARD-WITH HISTORY");
+            }
+            else{//std mode
+                mode=0;
+                advancebutton.setText("ADVANCE-WITH HISTORY");
+                historyText.setText(" ");
+                calobj.historyArray.clear();
+            }
+        }
+        else if (v == buttonequal) {
+            result = calobj.calculate();
+            calobj.printarray();
+            text = text + " = " + result;
+            if(mode == 1){ // For History Mode
+                calobj.pushtoHistoryArray(text);
+                for (String object:calobj.historyArray) {
+                    System.out.println("My History array is "+object+" ");
+                    histext = histext + object + "\n";
+                    historyText.setText(histext) ;
+                }
+            }
+            resultview.setText(text);
+        }
+        else {
+            text = text + buttontitle;
+            resultview.setText(text);
+            calobj.pushtoArray(buttontitle);
+            calobj.printarray();
+        }
+    }
+
 }
